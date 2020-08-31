0267f09ed118c7b1eeb65a43104d4bde1981dffa
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-08-31 13:35:24 +0300

    - Update .gitignore file

---
+++ build/precache-manifest.095843584bbe7835140b6eee26aec06f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172f9cf4b4914b9ddece36ab755bfd5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3304899a5a5b6b2cf9f0",
+    "url": "/static/css/main.09d81f44.chunk.css"
+  },
+  {
+    "revision": "f140aa4a10367f41e498",
+    "url": "/static/js/2.7aea8fe3.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.7aea8fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3304899a5a5b6b2cf9f0",
+    "url": "/static/js/main.6ad89912.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
