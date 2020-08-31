c62ccac7307b6642084dc7f7211d821c645943f1
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-08-31 13:14:52 +0300

    Bugfix: Import path module inside app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const path = require('path')
 const express = require('express')
 require('express-async-errors')
 const mongoose = require('mongoose')
